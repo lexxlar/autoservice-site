0fc03de1ad74e05670535972908e541b6b42e83d
Aleksandr Lekslar <lexxlar05@gmail.com>
2026-04-07 21:05:37 +0300

    Работа с шапкой страницы

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,16 @@
 <?php
 session_start();
 
-include_once '/includes/db.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/db.php';
+$user_id = $_SESSION["user_id"] ?? null;
+$user_role = $_SESSION['role'] ?? null;
+if ($user_id != null) {
+    $stmt = $pdo->prepare("SELECT first_name FROM users WHERE id = ?");
+    $stmt->execute([$user_id]);
+    $user_data = $stmt->fetch();
+    $user_name = $user_data['first_name'] ?? 'Пользователь';
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -12,6 +21,64 @@ include_once '/includes/db.php';
     <title>Document</title>
 </head>
 <body>
-    
-</body>
-</html>
+    <!--
+    <nav class="navbar navbar-expand-md navbar-light">
+        <div class="container-fluid">
+            <a href="#" class="navbar-brand">Монарх</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span> 
+            
+        </div>
+    </nav> -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Монарх</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Каталог</a>
+                </li>
+                <?php
+                if ($user_role === null) {
+                    echo '
+                    <div class="d-flex">
+                        <li class="nav-item">
+                            <a class="nav-link"  href="/login.php">Вход</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/register.php">Зарегистрироваться</a>
+                        </li>
+                    </div>
+                ';
+                } else if ($user_role === 'buyer'){
+                    echo '
+                    <li class="nav-item">
+                        <a class="nav-link" href="/user_panel.php">' . htmlspecialchars($user_name) . '</a>
+                    </li>
+                    <li class="nav-item" style="position: absolute; right: 50px;">
+                        <a class="nav-link" href="/logout.php">Выход</a>
+                    </li>
+                ';
+                } else if ($user_role === 'admin'){
+                    echo '
+                    <div class="d-flex">
+                        <li class="nav-item">
+                            <a class="nav-link"  href="/user_panel.php">Имя пользователя</a>
+                        </li>
+                    </div>
+                    <li class="nav-item">
+                        <a class="nav-link"  href="/admin/index.php">Админ-панель</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/logout.php">Выход</a>
+                    </li>
+                ';
+                }
+                ?>
+                </ul>
+            </div>
+        </div>
+    </nav>
