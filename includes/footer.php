404362ccfe352ed5b8a01207d472dd8479a7fa65
Aleksandr Lekslar <lexxlar05@gmail.com>
2026-04-08 11:59:45 +0300

    Добавление подвала сайта

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,10 @@
 
 
+<footer class="mt-5 py-3 bg-light text-center">
+    <div class="container">
+        <span class="text-muted">&copy; 2026 Автосалон Монарх</span>
+    </div>
+</footer>
 
-</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -20,12 +20,11 @@ if ($user_id != null) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Document</title>
 </head>
 <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
+        <div class="container">
             <a class="navbar-brand" href="#">Монарх</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
